144fc867010219a6f915a2e7713d9265a9216ea0
TanayDeakin <singhtanay0409@gmail.com>
2025-04-25 11:55:01 +1000

    Updated Files

--- packages/target_follower/src/target_follower.py
+++ packages/target_follower/src/target_follower.py
@@ -2,35 +2,26 @@
 
 import rospy
 from duckietown_msgs.msg import Twist2DStamped
-from duckietown_msgs.msg import FSMState
 from duckietown_msgs.msg import AprilTagDetectionArray
 
 class Target_Follower:
     def __init__(self):
-        
-        #Initialize ROS node
         rospy.init_node('target_follower_node', anonymous=True)
-
-        # When shutdown signal is received, we run clean_shutdown function
         rospy.on_shutdown(self.clean_shutdown)
-        
-        ###### Init Pub/Subs. REMEMBER TO REPLACE "akandb" WITH YOUR ROBOT'S NAME #####
+
         self.cmd_vel_pub = rospy.Publisher('/robot/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
         rospy.Subscriber('/robot/apriltag_detector_node/detections', AprilTagDetectionArray, self.tag_callback, queue_size=1)
-        ################################################################
 
-        rospy.spin() # Spin forever but listen to message callbacks
+        self.tag_detected = False  # To keep track of current state (object found or not)
+        rospy.spin()
 
-    # Apriltag Detection Callback
     def tag_callback(self, msg):
         self.move_robot(msg.detections)
- 
-    # Stop Robot before node has shut down. This ensures the robot keep moving with the latest velocity command
+
     def clean_shutdown(self):
         rospy.loginfo("System shutting down. Stopping robot...")
         self.stop_robot()
 
-    # Sends zero velocity to stop the robot
     def stop_robot(self):
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
@@ -39,27 +30,34 @@ class Target_Follower:
         self.cmd_vel_pub.publish(cmd_msg)
 
     def move_robot(self, detections):
-
-        #### YOUR CODE GOES HERE ####
-
-        if len(detections) == 0:
-            return
-
-        x = detections[0].transform.translation.x
-        y = detections[0].transform.translation.y
-        z = detections[0].transform.translation.z
-
-        rospy.loginfo("x,y,z: %f %f %f", x, y, z)
-
-
-        # Publish a velocity
         cmd_msg = Twist2DStamped()
         cmd_msg.header.stamp = rospy.Time.now()
-        cmd_msg.v = 0.0
-        cmd_msg.omega = 0.0
-        self.cmd_vel_pub.publish(cmd_msg)
 
-        #############################
+        if len(detections) == 0:
+            # No tag detected: SEEK MODE
+            rospy.loginfo("No object detected. Seeking...")
+            cmd_msg.v = 0.0
+            cmd_msg.omega = 1.0  # Rotate in place
+            self.tag_detected = False
+        else:
+            # Tag detected: LOOK AT OBJECT
+            tag = detections[0]
+            x = tag.transform.translation.x
+            rospy.loginfo("Object detected at x: %.2f", x)
+
+            cmd_msg.v = 0.0
+
+            # Determine angular velocity to center the tag
+            if abs(x) < 0.05:
+                cmd_msg.omega = 0.0  # Tag is centered
+                rospy.loginfo("Object centered.")
+            else:
+                cmd_msg.omega = -1.0 * x  # Rotate proportionally to x offset
+                rospy.loginfo("Adjusting to center object...")
+
+            self.tag_detected = True
+
+        self.cmd_vel_pub.publish(cmd_msg)
 
 if __name__ == '__main__':
     try:
